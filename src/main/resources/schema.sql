ade7ac4dae1abfcee61cc9e80a29d5d79f75083a
박소정 <psj6414@naver.com>
2022-03-08 10:48:36 +0900

    catalog-service init commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,3 @@
+insert into catalog(product_id, product_name, stock, unit_price) values ('000001', 'test1', 10, 10000);
+insert into catalog(product_id, product_name, stock, unit_price) values ('000002', 'test2', 11, 16000);
+insert into catalog(product_id, product_name, stock, unit_price) values ('000003', 'test3', 3, 8000);
